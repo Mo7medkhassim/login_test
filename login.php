743f5565af4261ce8b5dd5dd52dcabce87523c52
Mohammed Alkhassim <kassimsd350@gmail.com>
2021-09-09 20:20:52 +0400

    test

--- login.php
+++ login.php
@@ -16,20 +16,19 @@ $_SESSION['greeting'] = "You are Loging Successfully!";
 
 // data send by user
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $formUsername = $_POST['username'];
     $formPassword = $_POST['password'];
-    
+
     if ($formUsername !== $username) {
         $formError = ['username' => 'The username was not found.'];
     } elseif (!password_verify($formPassword, $passwordHash)) {
         $formError = ['password' => 'The provided password is invalid.'];
     }
 
-    if($formUsername == $username && $formPassword == $passwordHash){
+    if ($formUsername == $username && $formPassword == $passwordHash) {
         header('Location:home.php');
-
     }
 }
 
@@ -64,11 +63,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
+
 
 
             </ul>
@@ -81,33 +78,37 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     </nav>
 
 
+    <div class="container">
+        <div class="row">
 
-    <div class="d-flex justify-content-center mt-3">
-        <form method="post" action="" style="width: 100%; max-width: 350px;">
-            <div class="text-center mb-4">
-                <h1 class="h3 mb-3 font-weight-normal">Authenticate</h1>
-                <p>Use <code>admin</code> for both username and password.</p>
+
+            <div  class="d-flex justify-content-center mx-5 mt-3">
+                <form method="post" action="" class="px-5" style="width: 100%; max-width: 350px;">
+                    <div class="text-center mb-4">
+                        <h1 class="h3 mb-3 font-weight-normal">Authenticate</h1>
+                        <p>Use <code>admin</code> for both username and password.</p>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+
+                    <button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Login</button>
+                </form>
+
             </div>
 
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-
-            <button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Login</button>
-        </form>
-
+        </div>
     </div>
-
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
